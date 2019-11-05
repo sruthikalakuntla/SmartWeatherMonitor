f5c2ed4f68da463183d0bf6ea4a0f7594cdba799
sruthi kalakuntla <48819010+sruthikalakuntla@users.noreply.github.com>
2019-11-05 14:50:15 +0530

    Add files via upload

---
+++ SmartWeatherMonitor.ino
@@ -0,0 +1,83 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+//-------- Customise these values -----------
+const char* ssid = "YOUJNTU1S6";
+const char* password = "YOUJNTU1S6";
+#include "DHT.h"
+#define DHTPIN D2    // what pin we're connected to
+#define DHTTYPE DHT11   // define type of sensor DHT 11
+DHT dht (DHTPIN, DHTTYPE);
+ 
+#define ORG "zi9vak"
+#define DEVICE_TYPE "nodemcu"
+#define DEVICE_ID "12345"
+#define TOKEN "123456789"
+//-------- Customise the above values --------
+ 
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char publishtopic[] = "iot-2/evt/dhtdata/fmt/json";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+ 
+WiFiClient wifiClient;
+PubSubClient client(server, 1883,wifiClient);
+
+void setup() {
+ Serial.begin(115200);
+ Serial.println();
+ dht.begin();
+ Serial.print("Connecting to ");
+ Serial.print(ssid);
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+ delay(500);
+ Serial.print(".");
+ } 
+ Serial.println("");
+ 
+ Serial.print("WiFi connected, IP address: ");
+ Serial.println(WiFi.localIP());
+}
+ 
+void loop() {
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+
+if (isnan(h) || isnan(t))
+{
+Serial.println("Failed to read from DHT sensor!");
+delay(1000);
+return;
+}
+PublishData(t,h);
+delay(1000);
+}
+
+
+
+void PublishData(float temp, float humid){
+ if (!!!client.connected()) {
+ Serial.print("Reconnecting client to ");
+ Serial.println(server);
+ while (!!!client.connect(clientId, authMethod, token)) {
+ Serial.print(".");
+ delay(500);
+ }
+ Serial.println();
+ }
+  String payload = "{\"d\":{\"temperature\":";
+  payload += temp;
+  payload+="," "\"humidity\":";
+  payload += humid;
+  payload += "}}";
+ Serial.print("Sending payload: ");
+ Serial.println(payload);
+  
+ if (client.publish(publishtopic, (char*) payload.c_str())) {
+ Serial.println("Publish ok");
+ } else {
+ Serial.println("Publish failed");
+ }
+}
